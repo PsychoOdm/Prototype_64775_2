19d3b083938332d1716b9225eea4786a9ebbbb74
PsychoOdm <escolamarianacosta124@gmail.com>
2026-03-10 12:23:39 +0000

    AcçõesDeEntrada
    Todos desafios feitos, menos  gamepad pq nao tenho.

--- Assets/Scripts/PlayerControlle.cs
+++ Assets/Scripts/PlayerControlle.cs
@@ -3,48 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerControllerTutorialUpdates : MonoBehaviour
 {
-// Start is called before the first frame update
-void Start()
-{
+    public InputAction LeftAction;
+    public InputAction MoveAction;
 
-
-}
-
-
-// Update is called once per frame
-void Update()
-{
-    float horizontal = 0.0f;
-    if (Keyboard.current.leftArrowKey.isPressed)
+    void Start()
     {
-        horizontal = -1.0f;
- 	    }
-    else if (Keyboard.current.rightArrowKey.isPressed)
-    {
-        horizontal = 1.0f;
+         MoveAction.Enable(); 
+        LeftAction.Enable();
+       
     }
-    Debug.Log(horizontal);
 
+   void Update()
+ 
+   {
+     Vector2 move = MoveAction.ReadValue<Vector2>();
+     Debug.Log(move);
+     Vector2 position = (Vector2)transform.position + move * 0.1f;
+     transform.position = position;
+   
 
-    float vertical = 0.0f;
-    if (Keyboard.current.upArrowKey.isPressed)
-    {
-        vertical = 1.0f;
+        /*
+        // 
+        float horizontal = 0.0f;
+
+        if (LeftAction.IsPressed())
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+
+        Debug.Log(horizontal);
+
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+
+        Debug.Log(vertical);
+
+        Vector2 positionOld = transform.position;
+        positionOld.x += 0.1f * horizontal;
+        positionOld.y += 0.1f * vertical;
+        transform.position = positionOld;
+        */
     }
-    else if (Keyboard.current.downArrowKey.isPressed)
-    {
-        vertical = -1.0f;
-    }
-    Debug.Log(vertical);
-
-
-    Vector2 position = transform.position;
-    position.x = position.x + 0.1f * horizontal;
-    position.y = position.y + 0.1f * vertical;
-    transform.position = position;
-}
-
 }
