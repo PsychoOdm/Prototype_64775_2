8cc2facbb8c2ecdae790ade781d87248710e7aaa
PsychoOdm <escolamarianacosta124@gmail.com>
2026-04-02 14:59:52 +0100

    Final com Builds

--- Assets/Scripts/EnemyController_H.cs
+++ Assets/Scripts/EnemyController_H.cs
@@ -4,46 +4,78 @@ using UnityEngine;
 
 public class EnemyController_H : MonoBehaviour
 {
-   public float speed;
-   public float changeTime = 3.0f;
-  
-   Rigidbody2D rigidbody2d;
-   Animator animator;
-   float timer;
-   int direction = 1;
+    public float speed;
+    public float changeTime = 3.0f;
+    public ParticleSystem smokeEffect;
 
-   void Start()
-   {
-       rigidbody2d = GetComponent<Rigidbody2D>();
-       animator = GetComponent<Animator>();
-       timer = changeTime;
-   }
+    Rigidbody2D rigidbody2d;
+    Animator animator;
+    float timer;
+    int direction = 1;
+    bool broken = true;
 
-   void Update()
-   {
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        timer = changeTime;
+    }
+
+    void Update()
+    {
         timer -= Time.deltaTime;
+
         if (timer < 0)
         {
             direction = -direction;
             timer = changeTime;
         }
-   }
+    }
 
-   void FixedUpdate()
-   {
-       Vector2 position = rigidbody2d.position;
-       position.x = position.x + speed * direction * Time.deltaTime;
-       animator.SetFloat("Move X", direction);
-       animator.SetFloat("Move Y", 0);
-       rigidbody2d.MovePosition(position);
-   }
+    void FixedUpdate()
+    {
+        if (!broken)
+        {
+            return;
+        }
 
-   void OnTriggerEnter2D(Collider2D other)
-   {
-       PlayerController player = other.GetComponent<PlayerController>();
-       if (player != null)
-       {
-           player.ChangeHealth(-1);
-       }
-   }
+        Vector2 position = rigidbody2d.position;
+
+        position.x = position.x + speed * direction * Time.deltaTime;
+        animator.SetFloat("Move X", direction);
+        animator.SetFloat("Move Y", 0);
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        GetComponent<Rigidbody2D>().simulated = false;
+        animator.SetTrigger("Fixed");
+
+        if (smokeEffect != null)
+        {
+            smokeEffect.Stop();
+        }
+    }
 }
--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -48,7 +48,7 @@ public class UIHandler : MonoBehaviour
 
     public void SetHealthValue(float percentage)
     {
-        m_Healthbar.style.width = Length.Percent(100 * percentage);
+        m_Healthbar.style.width = Length.Percent(55 * percentage);
     }
 
     public void DisplayDialogue()
